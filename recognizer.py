58e84e2a74b54973179e6817d1b42a6ed3e51d03
cenckov <32960276+cenckov@users.noreply.github.com>
2019-06-26 11:07:27 +0200

    Update recognizer.py

--- recognizer.py
+++ recognizer.py
@@ -1,21 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 17 21:19:16 2019
-
-@author: acencek
-"""
-
 import speech_recognition as sr
 
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    print('Powiedz cos! ')
+    print('Powiedz coś! ')
     audio = r.listen(source)
     
     try:  
         text = r.recognize_google(audio, language='pl')
-        print(f'Powiedziales: {text}')
+        print(f'Powiedziałeś: {text}')
     except:
-        print('Przepraszam, powtorz jeszcze raz')
-
+        print('Przepraszam, powtórz jeszcze raz')
